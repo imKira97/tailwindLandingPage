058fc34cb82ba2ec4f562b54e2fe96d0e45d9d8c
Shiva Pandey <codingshiva97@gmail.com>
2025-07-05 17:51:36 +0530

    complete

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -2,28 +2,26 @@ import React from 'react'
 
 const Body = () => {
   return (
-    <div>
-        <div className='flex items-center justify-center'>
-            <img src='./assets/Blue-Shape.svg' alt='1st' className=' -rotate-45 h-64'/>
-            <img src='./assets/Pink-Shape.svg' alt='2nd' className=' absolute -rotate-30 h-64'/>
-            <img src='./assets/Purple-Shape.svg' alt='3rd' className='absolute -rotate-15 h-64'/>
-            <img src='./assets/Hero-Model.png' alt='hero' className='absolute  h-64'/>
-        </div>
-        <div >
-            <h1 className='text-5xl font-bold font-mono  leading-tight'> Host your website in 5 minutes </h1>
-            <p className='font-serif text-gray-400 my-2' >With Hoster,get your website up and running in under 5 minutes with the most competitive pricing packages available online </p>
-            <form action="" className='flex flex-col gap-4'>
-                <input type='email' placeholder='Enter Your email' className='rounded-md px-4 py-3  bg-white placeholder:text-amber-800'></input>
-                <button className='rounded-md px-4 py-3 bg-blue-400 hover:bg-blue-800 text-white'>Join Waitlist</button>
+    <div className='lg:flex'>
 
-            </form>
-            <div className='flex gap-2'>
-                <img src='./assets/Checkmark.svg' alt='check'/>
-                <p className='font-mono text-gray-500'>No spam ever.Unsubscribe anytime</p>
-            </div>
-        </div>
-
-
+    <div className='flex items-center justify-center lg:flex-1 lg:order-2 lg:justify-end lg:overflow-hidden'>
+      <img src="./assets/Blue-Shape.svg" alt="img1" className='h-64 -rotate-45 md:h-72 lg:h-[450px] lg:overflow-hidden'/>
+      <img src="./assets/Pink-Shape.svg" alt="img2" className='absolute h-64 -rotate-[30deg]  md:h-72 lg:h-[450px] lg:overflow-hidden'/>
+      <img src="./assets/Purple-Shape.svg" alt="img3" className='absolute h-64 -rotate-[15deg]  md:h-72 lg:h-[450px] lg:overflow-hidden'/>
+      <img src="./assets/Hero-Model.png" alt="hero" className='absolute h-64  md:h-72 lg:h-[450px] lg:overflow-hidden'/>
+    </div>
+    <div className='space-y-4 lg:flex-1 lg:order-1 lg:space-y-10'>
+      <h1 className='text-4xl font-bold leading-tight font-playfair '>Host your website in less than 5 minutes</h1>
+      <p className='font-lato text-gray-400'>With Hoster , get your website up and running in no less than 5 minutes with the most competitive pricing packages available online.</p>
+      <form action="" className='flex flex-col gap-4 md:flex-row'>
+        <input className='rounded-md px-4 py-3 placeholder:text-gray-400 bg-white' type="email" placeholder='Enter e-mail address' />
+        <button className='rounded-md px-4 py-3 bg-blue-400 hover:bg-blue-600 text-white'>Join Waitlist</button>
+      </form>
+      <div className='flex gap-2'>
+        <img src="./assets/Checkmark.svg" alt="lg" />
+        <p className='font-lato text-gray-400'>No spam ,ever . Unsubscribe anytime</p>
+      </div>
+    </div>
     </div>
   )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
     </div>
     <div className='lg:hidden'>
     <FaBars/>
-    </div>
+    </div> 
     </div>
   )
 }
